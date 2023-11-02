79c40084c60e108032c0fa64e447e1cefedec588
emiliano <emiliontn@hotmail.com>
2023-11-01 19:26:56 -0600

    subir proyecto

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 origins = ["*"]
 
-app = FastAPI(title='Heart Disease Prediction')
+app = FastAPI(title='Heart failure Prediction')
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,29 +17,32 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-model = load(pathlib.Path('model/heart-disease-v1.joblib'))
+model = load(pathlib.Path('model/heart_failure_clinical_records_dataset-v1.joblib'))
 
 class InputData(BaseModel):
-    age: int = 64
-    sex: int = 1
-    cp: int = 3
-    trestbps: int = 120
-    chol: int = 267
-    fbs: int = 0
-    restecg: int = 0
-    thalach: int = 99
-    exang: int = 1
-    oldpeak: float = 1.8
-    slope: int = 1
-    ca: int = 2
-    thal: int = 2
+    age: int = 75
+    anaemia: int = 1
+    creatinine_phosphokinase: int = 582
+    diabetes: int = 0
+    ejection_fraction: int = 38
+    high_blood_pressure: int = 1
+    platelets: int = 265000
+    serum_creatinine: float = 9.4
+    serum_sodium: int = 140
+    sex: int = 0
+    smoking: int = 1
+    time: int = 4
 
 class OutputData(BaseModel):
-    score: float = 0.80318881046519
+    prediction: int
+    probability: float 
 
 @app.post('/score', response_model=OutputData)
 def score(data: InputData):
     model_input = np.array([v for k, v in data.dict().items()]).reshape(1, -1)
-    result = model.predict_proba(model_input)[:, -1]
+    probability = model.predict_proba(model_input)[:, -1]
+    
+    # Realiza una predicción binaria (1 o 0) a partir de la probabilidad
+    prediction = 1 if probability >= 0.5 else 0
 
-    return {'score': result}
+    return {'prediction': prediction, 'probability': probability}
--- model/train.py
+++ model/train.py
@@ -1,21 +1,31 @@
-from os import PathLike
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import classification_report
 from joblib import dump
 import pandas as pd
 import pathlib
 
-df = pd.read_csv(pathlib.Path('data/heart-disease.csv'))
-y = df.pop('target')
+df = pd.read_csv(pathlib.Path('data/heart_failure_clinical_records_dataset.csv'))
+y = df.pop('DEATH_EVENT')
 X = df
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-print ('Training model.. ')
-clf = RandomForestClassifier(n_estimators = 10,
-                            max_depth=2,
-                            random_state=0)
+# Ajuste de hiperparámetros con Grid Search
+param_grid = {
+    'n_estimators': [10, 50, 100],
+    'max_depth': [2, 5, 10]
+}
+
+clf = GridSearchCV(RandomForestClassifier(random_state=0), param_grid, cv=5)
 clf.fit(X_train, y_train)
-print ('Saving model..')
 
-dump(clf, pathlib.Path('model/heart-disease-v1.joblib'))
+# Imprime el mejor conjunto de hiperparámetros
+print('Mejor conjunto de hiperparámetros:')
+print(clf.best_params_)
+
+# Guarda el modelo entrenado
+dump(clf, pathlib.Path('model/heart_failure_clinical_records_dataset-v1.joblib'))
+
+# Evalúa el modelo en el conjunto de prueba
+y_pred = clf.predict(X_test)
+print(classification_report(y_test, y_pred))
